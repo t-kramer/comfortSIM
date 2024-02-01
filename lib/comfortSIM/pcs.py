b48505b097a1ff19ce29d3249fc849febfe01d10
t2the0bi <49641232+t2the0bi@users.noreply.github.com>
2024-01-31 20:22:15 -0800

    Work on functions

--- lib/comfortSIM/pcs.py
+++ lib/comfortSIM/pcs.py
@@ -1,21 +1,68 @@
-# def function_name(arg1, arg2, arg3='default_value'):
-#     """
-#     Brief description of the function.
+import numpy as np
 
-#     Detailed description of the function.
+class Environment:
+    """
+    A class used to represent an Environment.
 
-#     Parameters:
-#     arg1 (type): Description of arg1.
-#     arg2 (type): Description of arg2.
-#     arg3 (type, optional): Description of arg3. Defaults to 'default_value'.
+    ...
 
-#     Returns:
-#     type: Description of return value.
-#     """
+    Attributes
+    ----------
+    parameters : list
+        A list of parameters that define the environment.
+    climate : np.array
+        A 2D numpy array with shape (4,n) representing the climate data. 
+        Each row corresponds to a different environmental factor (ta, rh, tr, vel).
 
-#     # Function implementation goes here
-#     pass
+    Methods
+    -------
+    
+    """
+
+    def __init__(self, parameters:list, climate:np.array):
+        self.parameters = parameters
+        self.climate = climate
 
 
-# class defintions
+class ComfortPreference:
+
+    def __init__(self, preference:np.array):
+        self.preference = preference    # np.array shape (1,n) with comfort preference prediction
+
+    def predict_preference():
+        # return prediction, implement use of ML here
+        pass
+
+
+class Device:
+    def __init__(self, type, power:int, effect:np.array):
+        self.type = type        # heater, cooler or fan
+        self.power = power      # e.g. 200W
+        self.effect = effect    # e.g. [2, 0, 1.2, 0] for effect on ta, rh, tr, vel
+
+    def use_device(self, environment, comfort):
+        # apply effect array to climate array according to comfort preference
+
+        new_climate = np.copy(environment.climate)
+
+        for i in range(len(comfort.preference)):
+
+            effect_ratio = abs(comfort.preference[i])/1
+
+            if self.type == 'fan' and comfort.preference[i] < 0:
+                air_movement = effect_ratio * self.effect
+                new_climate[:, i] += air_movement
+
+            elif self.type == 'cooler' or self.type == 'fan' and comfort.preference[i] < 0:
+                cooling = effect_ratio * self.effect
+                new_climate[:, i] += cooling
+
+            elif self.type == 'heater' and comfort.preference[i] > 0:
+                heating = effect_ratio * self.effect
+                new_climate[:, i] += heating
+
+        environment.climate = new_climate   # update climate array
+
+        # return new_climate    as alternative to updating climate array
+        # pass
 
--- lib/comfortSIM/postprocess.py
+++ lib/comfortSIM/postprocess.py
@@ -1,29 +1,57 @@
-# def function_name(arg1, arg2, arg3='default_value'):
-#     """
-#     Brief description of the function.
+# This file contains the post-processing functions for the comfortSIM package.
 
-#     Detailed description of the function.
+import numpy as np
 
-#     Parameters:
-#     arg1 (type): Description of arg1.
-#     arg2 (type): Description of arg2.
-#     arg3 (type, optional): Description of arg3. Defaults to 'default_value'.
-
-#     Returns:
-#     type: Description of return value.
-#     """
-
-#     # Function implementation goes here
-#     pass
-
-
-
-def compute_sta():
+def predict_spatial_thermal_preference(offsetArrays, environmentalArray, predictionModel):
     """
-    Compute sTA based on simulated indoor environmental data.
+    Predicts spatial thermal preference based on environmental variables and offset arrays.
+
+    Args:
+        offsetArrays (numpy.ndarray): _description_
+        environmentalArray (numpy.ndarray): _description_
+        predictionModel (sklearn.BaseEstimator): _description_
+
+    Returns:
+        combinedPrediction (numpy.ndarray): _description_
+    """
+    
+    ta, rh, tr, vel = np.split(environmentalArray, 4, axis=0)
+
+    predictionArrays = [] # list to store the prediction arrays for each offset class
+
+    # loop through offset arrays
+    for offsetArray in offsetArrays:
+
+        flattenedSimResults = np.vstack((ta.flatten(), rh.flatten(), tr.flatten(), vel.flatten(), offsetArray.flatten()))
+        inputDf = pd.DataFrame(flattenedSimResults.transpose(), columns=['ta','rh','tr','vel','ts_offset_class']).round(1)
+
+        y_proba = predictionModel.predict_proba(inputDf)
+        predictionArrays.append(-y_proba[:,0] + y_proba[:,-1])
+
+    # combining the prediction arrays for each offset class
+    combinedPrediction = np.array(predictionArrays)
+
+    return combinedPrediction
+
+
+
+def compute_sta(prediction_array, percentage_area=0.5):
+    """
+    Computes sTA based on simulated indoor environmental data.
+
+    Args:
+        prediction_array (numpy.ndarray): 2D array containing predictions of spatial thermal preference.
+        percentage_area (float, optional): Percentage of area threshold for sTA; defaults to 0.5.
+
+    Returns:
+        comfort_hours (numpy.ndarray): 2D array containing a boolean comfort prediction for each grid point.
+        hourly_autonomy (numpy.ndarray): 1D array where each element represents the hourly sTA.
+        spatial_autonomy (float): Annual sTA depending on specified percentage area.
     """
 
-    pass
+    comfort_hours = np.where(abs(prediction_array) <= 0.5, 1, 0)
 
+    hourly_autonomy = np.sum(comfort_hours, axis=0) / prediction_array.shape[0]
+    spatial_autonomy = np.sum( np.where(hourly_autonomy > percentage_area, 1, 0)) / prediction_array.shape[1]
 
-
+    return spatial_autonomy, hourly_autonomy, comfort_hours
